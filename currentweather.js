094bf5cf5101bb377d2b1ca5c014c462db9e3afd
YenLun <yenlunliu@gmail.com>
2023-04-11 12:27:49 -0700

    getting user's location js

---
+++ currentweather.js
@@ -0,0 +1,11 @@
+// get users location //
+
+const successCallback = (position) => {
+    console.log(position);
+};
+
+const errorCallback = (error) => {
+    console.log(error);
+};
+
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
