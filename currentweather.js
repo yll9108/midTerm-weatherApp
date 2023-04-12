1222acf99ceb8f0598bb69de724c627f09659f53
YenLun <yenlunliu@gmail.com>
2023-04-12 14:21:09 -0700

    searching city API doesn't work

--- currentweather.js
+++ currentweather.js
@@ -12,18 +12,29 @@
 // fetch current weather API //
 
 const apiKey = "c77784ebb91710984ac4bdf2158e585d";
-const apiUrl =
-    "https://api.openweathermap.org/data/2.5/weather?lat=49.246292&lon=-123.116226&units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+const searchArea = document.querySelector(".searchBar");
+const searchBtn = document.querySelector(".searchBar + .searchBtn");
 
-async function currentWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
-    let currentData = await response.json();
+async function currentWeather(city) {
+    const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
+    const currentData = await response.json();
     console.log(currentData);
     document.getElementById("currentWeatherCity").innerHTML = currentData.name;
-    document.getElementById("currentWeatherTemp").innerHTML =
-        "Temp : " + currentData.main.temp + "°C ";
-    document.getElementById("currentWeatherFeel").innerHTML =
-        "Feels like .. " + currentData.main.feels_like + "°C ";
+    document.getElementById(
+        "currentWeatherTemp"
+    ).innerHTML = `Temp: ${currentData.main.temp}°C`;
+    document.getElementById(
+        "currentWeatherFeel"
+    ).innerHTML = `Feels like: ${currentData.main.feels_like}°C`;
 }
 
-currentWeather();
+if (searchBtn !== null) {
+    searchBtn.addEventListener("click", () => {
+        currentWeather(searchArea.value).catch((error) => {
+            console.error(error);
+        });
+    });
+} else {
+    // console.error("searchBtn is null");
+}
