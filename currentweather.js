3279de4964a235e8e05ae21449292af429d4cb5e
YenLun <yenlunliu@gmail.com>
2023-04-12 14:53:40 -0700

    searching bar is working

--- currentweather.js
+++ currentweather.js
@@ -11,30 +11,40 @@
 
 // fetch current weather API //
 
-const apiKey = "c77784ebb91710984ac4bdf2158e585d";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
-const searchArea = document.querySelector(".searchBar");
-const searchBtn = document.querySelector(".searchBar + .searchBtn");
+window.addEventListener("load", () => {
+    // windows會等待html跑完再執行js. //
+    const apiKey = "c77784ebb91710984ac4bdf2158e585d";
+    const apiUrl =
+        "https://api.openweathermap.org/data/2.5/weather?units=metric";
+    const searchArea = document.querySelector(".searchBar");
+    const searchBtn = document.querySelector(".searchBtn");
 
-async function currentWeather(city) {
-    const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
-    const currentData = await response.json();
-    console.log(currentData);
-    document.getElementById("currentWeatherCity").innerHTML = currentData.name;
-    document.getElementById(
-        "currentWeatherTemp"
-    ).innerHTML = `Temp: ${currentData.main.temp}°C`;
-    document.getElementById(
-        "currentWeatherFeel"
-    ).innerHTML = `Feels like: ${currentData.main.feels_like}°C`;
-}
+    async function currentWeather(city) {
+        const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
+        const currentData = await response.json();
+        console.log(currentData);
+        document.getElementById("currentWeatherCity").innerHTML =
+            currentData.name;
+        document.getElementById(
+            "currentWeatherTemp"
+        ).innerHTML = `Temp: ${currentData.main.temp}°C`;
+        document.getElementById(
+            "currentWeatherFeel"
+        ).innerHTML = `Feels like: ${currentData.main.feels_like}°C`;
+    }
 
-if (searchBtn !== null) {
-    searchBtn.addEventListener("click", () => {
-        currentWeather(searchArea.value).catch((error) => {
-            console.error(error);
-        });
+    currentWeather("Vancouver").catch((error) => {
+        // 預設city是Vancouver //
+        console.error(error);
     });
-} else {
-    // console.error("searchBtn is null");
-}
+
+    if (searchBtn !== null) {
+        searchBtn.addEventListener("click", () => {
+            currentWeather(searchArea.value).catch((error) => {
+                console.error(error);
+            });
+        });
+    } else {
+        console.error("searchBtn is null");
+    }
+});
