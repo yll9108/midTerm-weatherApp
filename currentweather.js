0afd659d0d9d3fa02447df60d2bdd1d6d08450f0
YenLun <yenlunliu@gmail.com>
2023-04-12 13:12:28 -0700

    default city(VAN) current weather API js done

--- currentweather.js
+++ currentweather.js
@@ -1,10 +1,29 @@
 // get users location //
-const successCallback = (position) => {
-    console.log(position);
-};
+// const successCallback = (position) => {
+//     console.log(position);
+// };
 
-const errorCallback = (error) => {
-    console.log(error);
-};
+// const errorCallback = (error) => {
+//     console.log(error);
+// };
 
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+// navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+// fetch current weather API //
+
+const apiKey = "c77784ebb91710984ac4bdf2158e585d";
+const apiUrl =
+    "https://api.openweathermap.org/data/2.5/weather?lat=49.246292&lon=-123.116226&units=metric";
+
+async function currentWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    let currentData = await response.json();
+    console.log(currentData);
+    document.getElementById("currentWeatherCity").innerHTML = currentData.name;
+    document.getElementById("currentWeatherTemp").innerHTML =
+        "Temp : " + currentData.main.temp + "°C ";
+    document.getElementById("currentWeatherFeel").innerHTML =
+        "Feels like .. " + currentData.main.feels_like + "°C ";
+}
+
+currentWeather();
