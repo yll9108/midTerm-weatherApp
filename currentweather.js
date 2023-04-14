67490fae5cc23a63d0e212d0d52bb274d2151b40
YenLun <yenlunliu@gmail.com>
2023-04-14 09:45:14 -0700

    getting user location and apply on current weather done

--- currentweather.js
+++ currentweather.js
@@ -1,54 +1,47 @@
 // get users location //
-
 // creating fetch API to make a request //
+// when the requested is completed, using DOM to manipulate the user interface //
 
 const apiKey = "c77784ebb91710984ac4bdf2158e585d";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
-// when the requested is completed, using DOM to manipulate the user interface //
-
-function getUserWeather() {
+function getUserWeather(latitude, longitude) {
     let userCity = document.getElementById("currentWeatherCity");
     let userTemp = document.getElementById("currentWeatherTemp");
     let userFeel = document.getElementById("currentWeatherFeel");
-}
-
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
-function successCallback(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
 
     async function currentUserLocation() {
-        const response = await fetch(`${apiUrl}&q=${position}&appid=${apiKey}`);
+        const response = await fetch(
+            `${apiUrl}&lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+        );
         const currentUserData = await response.json();
         console.log(currentUserData);
-        document.getElementById("currentWeatherCity").innerHTML =
-            currentData.name;
-        document.getElementById(
-            "currentWeatherTemp"
-        ).innerHTML = `Temp: ${currentData.main.temp}°C`;
-        document.getElementById(
-            "currentWeatherFeel"
-        ).innerHTML = `Feels like: ${currentData.main.feels_like}°C`;
+        userCity.innerHTML = currentUserData.name;
+        userTemp.innerHTML = `Temp: ${currentUserData.main.temp}°C`;
+        userFeel.innerHTML = `Feels like: ${currentUserData.main.feels_like}°C`;
     }
+
+    currentUserLocation().catch((error) => {
+        console.error(error);
+    });
+}
+
+function successCallback(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    getUserWeather(latitude, longitude);
 }
 
 function errorCallback() {
     console.log("error");
 }
 
-// navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-
-getUserWeather();
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
 // fetch current weather API //
 
 window.addEventListener("load", () => {
     // windows會等待html跑完再執行js. //
-    const apiKey = "c77784ebb91710984ac4bdf2158e585d";
-    const apiUrl =
-        "https://api.openweathermap.org/data/2.5/weather?units=metric";
     const searchArea = document.querySelector(".searchBar");
     const searchBtn = document.querySelector(".searchBtn");
 
@@ -67,7 +60,6 @@ window.addEventListener("load", () => {
     }
 
     currentWeather("London").catch((error) => {
-        // 預設city是Vancouver //
         console.error(error);
     });
 
