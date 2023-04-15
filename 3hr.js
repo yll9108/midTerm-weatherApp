a63da05739a25d514a97d79462920bc9f757b0f5
Bosco Lam <121968040+bosco1230@users.noreply.github.com>
2023-04-15 10:02:17 -0700

    searchbar to change weather forecast

--- 3hr.js
+++ 3hr.js
@@ -1,21 +1,27 @@
-const apikey ="4d697593455ee5751089d0a14f15ecff";
-const apiurl ="https://api.openweathermap.org/data/2.5/forecast?units=metric&q=vancouver";
+document.addEventListener("DOMContentLoaded", () => {
+  const apikey ="4d697593455ee5751089d0a14f15ecff";
+  const apiurl ="https://api.openweathermap.org/data/2.5/forecast?units=metric&q=";
 
+  const searchBar = document.querySelector(".headerBar input");
+  const searchBtn = document.querySelector(".headerBar button");
 
-window.addEventListener("DOMContentLoaded", () => {
-async function forecast(){
-    const response = await fetch(apiurl + `&appid=${apikey}`);
-    var data = await response.json();
+  async function forecast(city){
+      const response = await fetch(apiurl + city + `&appid=${apikey}`);
+      var data = await response.json();
 
-    console.log(data);
+      console.log(data);
 
-    for (let i = 1; i <= 8; i++) {
-        document.querySelector(`#time${i}`).innerHTML = data.list[i-1].dt_txt;
-        document.querySelector(`#temp${i}`).innerHTML = "Temp:" + Math.round(data.list[i-1].main.temp) + "&deg" + "C";
-        document.querySelector(`#feel${i}`).innerHTML = "Feels Like:" + Math.round(data.list[i-1].main.feels_like) + "&deg" + "C";
-      }
-      
-}
+      for (let i = 1; i <= 8; i++) {
+          document.querySelector(`#time${i}`).innerHTML = data.list[i-1].dt_txt;
+          document.querySelector(`#temp${i}`).innerHTML = "Temp:" + Math.round(data.list[i-1].main.temp) + "&deg" + "C";
+          document.querySelector(`#feel${i}`).innerHTML = "Feels Like:" + Math.round(data.list[i-1].main.feels_like) + "&deg" + "C";
+        }
+        
+  }
 
-forecast()
+  searchBtn.addEventListener("click", ()=>{
+    forecast(searchBar.value);
+  })
+
+  forecast("Vancouver");
 });
