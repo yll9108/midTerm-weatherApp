d2cfc5567b0e9b81aed0cb18fe2c98aa0c2c311d
Kevin Lee <kevinlfcola@gmail.com>
2023-04-16 20:29:24 -0700

    switching branch commit

--- 3hr.js
+++ 3hr.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const searchBar = document.querySelector(".headerBar input");
     const searchBtn = document.querySelector(".headerBar button");
+    const conditionIcon = document.querySelector("#condition-icon");
+    const condition = document.querySelector("#condition");
 
     async function forecast(city) {
         const response = await fetch(apiurl + city + `&appid=${apikey}`);
@@ -22,6 +24,35 @@ document.addEventListener("DOMContentLoaded", () => {
                 Math.round(data.list[i - 1].main.feels_like) +
                 "&deg" +
                 "C";
+
+            if (
+                data.list[i - 1].weather[0].main == "Rain" ||
+                data.list[i - 1].weather[0].description == "light rain"
+            ) {
+                document.querySelector(`#condition-icon${i}`).src =
+                    "weather_images/rain.webp";
+                document.querySelector(`#condition${i}`).innerHTML = "Raining";
+            } else if (data.list[i - 1].weather[0].main == "Snow") {
+                document.querySelector(`#condition-icon${i}`).src =
+                    "weather_images/snow.webp";
+                document.querySelector(`#condition${i}`).innerHTML = "Snowing";
+            } else if (data.list[i - 1].weather[0].main == "Clouds") {
+                document.querySelector(`#condition-icon${i}`).src =
+                    "weather_images/cloudy.png";
+                document.querySelector(`#condition${i}`).innerHTML = "Clouds";
+            } else if (data.list[i - 1].weather[0].main == "Clear") {
+                document.querySelector(`#condition-icon${i}`).src =
+                    "weather_images/sun.png";
+                document.querySelector(`#condition${i}`).innerHTML = "Clear";
+            } else if (
+                data.list[i - 1].weather[0].main == "Rain" ||
+                data.list[i - 1].weather[0].description == "moder"
+            ) {
+                document.querySelector(`#condition-icon${i}`).src =
+                    "weather_images/rain.webp";
+                document.querySelector(`#condition${i}`).innerHTML =
+                    "Moderate Rain";
+            }
         }
     }
 
